aceea875c8611927a0327d88621670ba142e5f17
CuSO4_Deposit <CuSO4D@protonmail.com>
2023-06-10 03:03:07 +0000

    AAAAA AAAA not fixed

--- src/1071_Greatest-Common-Divisor-of-Strings/first.c
+++ src/1071_Greatest-Common-Divisor-of-Strings/first.c
@@ -1,18 +1,21 @@
 char * gcdOfStrings(char * str1, char * str2){
 	int maxcommon = 0;
-	for (int i = 2; i <= strlen(str1) / 2; i++){
-		if (strlen(str1) % i == 0 && strlen(str2) % 2 == 0){
+	for (int i = 1; i <= strlen(str1) / 2; i++){
+		if (strlen(str1) % i == 0 && strlen(str2) % i == 0){
 			int num = strlen(str1) / i;
 			int flag = 1;
 			for (int j = 0; j < i; j++){
 				if (flag == 0){
 					break;
 				}
-				for (int k = 0; k < num; k++){
-					if (str1[j + k] != str1[j]){
+				for (int k = 1; k < num; k++){
+					if (str1[j + k * i] != str1[j]){
 						flag = 0;
 					}
 				}
+			}
+			if (flag == 0){
+				continue;
 			}
 			// reaches here means str1[0..num-1] is a divisor of str1
 			num = strlen(str2) / i;
@@ -20,19 +23,24 @@ char * gcdOfStrings(char * str1, char * str2){
 				if (flag == 0){
 					break;
 				}
-				for (int k = 0; k < num; k++){
-					if (str2[j + k] != str1[j]){
+				for (int k = 1; k < num; k++){
+					if (str2[j + k * i] != str2[j]){
 						flag = 0;
 					}
 				}
+			}
+			if (flag == 0){
+				continue;
 			}
 			// reaches here means str1[0..num-1] is a common divisor
 			maxcommon = i;
 		}
 	}
-	char* common_divisor = (char*)malloc(maxcommon * sizeof(char));
+	char* common_divisor = (char*)malloc(maxcommon * sizeof(char) + 1);
 	for (int i = 0; i < maxcommon; i++){
 		common_divisor[i] = str1[i];
 	}
+	common_divisor[maxcommon] = '\0';
 	return common_divisor;
 }
+
