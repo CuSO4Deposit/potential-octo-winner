58639fc7c94017e797d353c592309156d5a865f0
cuso4d <87959312+CuSO4Deposit@users.noreply.github.com>
2026-01-02 08:51:54 +0000

    66. Plus One

---
+++ src/66_Plus-One/1.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        digits = [0] + digits
+        digits[-1] += 1
+        for i in range(len(digits)):
+            if digits[-i-1] == 10:
+                digits[-i-1] = 0
+                digits[-i-2] += 1
+        if digits[0] == 0:
+            digits = digits[1:]
+        return digits
