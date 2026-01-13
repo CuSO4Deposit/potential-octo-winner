6f1ea48cc4dcbf6efd7ce094548a727fa34a3105
cuso4d <87959312+CuSO4Deposit@users.noreply.github.com>
2026-01-13 00:45:44 +0000

    wip

---
+++ src/865_Smallest-Subtree-with-all-the-Deepest-Nodes/1.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool traverse(TreeNode* root, void*(*func)()) {
+        if (!root) return false;
+        bool l = traverse(root->left, func);
+        bool r = traverse(root->right, func);
+    }
+
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        
+    }
+};
